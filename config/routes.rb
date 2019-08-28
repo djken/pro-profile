3fa80728cfb9de0500416cfe15c85831d433009d
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-08-28 15:48:12 -0400

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'users#index'
   get '/login', to: 'sessions#new', as: :new_session
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :destroy_session
